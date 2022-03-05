a905ebf85403be66d892e9288d121c13f71e05eb
j-cort <81960011+j-cort@users.noreply.github.com>
2022-03-05 11:28:08 +0000

    refactor printStatement: extract generating date, debit & credit values to separate functions

--- model/Printer.js
+++ model/Printer.js
@@ -3,13 +3,24 @@ class Printer {
   printStatement(transactions) {
     console.log("date || credit || debit || balance");
     for (let transaction of transactions) {
-      const date = transaction.date.toLocaleString().split(",")[0];
-      const credit = transaction.value > 0 ? transaction.value.toFixed(2).toString() : "";
-      const debit =
-        transaction.value < 0 ? Math.abs(transaction.value).toFixed(2).toString() : "";
+      const date = this.generateDate(transaction.date)
+      const credit = this.generateCredit(transaction.value)
+      const debit = this.generateDebit(transaction.value)
       const balance = transaction.balance.toFixed(2)
       console.log(`${date} || ${credit} || ${debit} || ${balance}`);
     }
+  }
+
+  generateDate(date) {
+    return date.toLocaleString().split(",")[0];
+  }
+
+  generateCredit(value) {
+    return value > 0 ? value.toFixed(2).toString() : "";
+  }
+
+  generateDebit(value) {
+    return value < 0 ? Math.abs(value).toFixed(2).toString() : "";
   }
 
 }
